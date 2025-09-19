f9505450867b76c8a4ee3258d461fada2d498e9b
Mukola Trotsenko <newdarkstudiot@gmail.com>
2025-09-19 16:40:53 +0300

    Update form.php

--- components/form/form.php
+++ components/form/form.php
@@ -2,141 +2,107 @@
 {% set recaptcha = __SELF__.recaptcha %}
 
 <div class="form-{{ form.code }}">
-	<p>{{ form.description|raw }}</p>
-	<form
-		data-request="onSubmitForm"
-		data-request-success="$(this).remove()"
-		data-request-update="'{{__SELF__}}::result': '.callback-form-result'"
-		data-request-files
-		action="POST"
-	>
-		<input type="hidden" name="form_id" value="{{ form.id }}">
-		{% for field in form.fields %}
+    <p>{{ form.description|raw }}</p>
+    <form
+        id="form-{{ form.code }}"
+        data-request="onSubmitForm"
+        data-request-success="$(this).remove()"
+        data-request-update="'{{__SELF__}}::result': '.callback-form-result'"
+        data-request-files
+        action="POST"
+    >
+        <input type="hidden" name="form_id" value="{{ form.id }}">
 
-			{# TEXT #}
-			{% if field.type == 'text' %}
-				<div class="form-group field-{{ field.code }}">
-					<label for="{{ field.code }}">{{ field.title }}</label>
-					<input class="form-control"	type="text"	name="{{ field.code }}"	id="{{ field.code }}" placeholder="{{ field.placeholder }}">
-				</div>
-			{# /TEXT #}
+        {% for field in form.fields %}
+            {% if field.type == 'text' %}
+                <div class="form-group field-{{ field.code }}">
+                    <label for="{{ field.code }}">{{ field.title }}</label>
+                    <input class="form-control" type="text" name="{{ field.code }}" id="{{ field.code }}" placeholder="{{ field.placeholder }}">
+                </div>
 
-			{# TEXTAREA #}
-			{% elseif field.type == 'textarea' %}
-				<div class="form-group field-{{ field.code }}">
-					<label for="{{ field.code }}">{{ field.title }}</label>
-					<textarea
-						class="form-control"
-						name="{{ field.code }}"
-						id="{{ field.code }}"
-						placeholder="{{ field.placeholder }}"
-						rows="7"
-					></textarea>
-				</div>
-			{# /TEXTAREA #}
+            {% elseif field.type == 'textarea' %}
+                <div class="form-group field-{{ field.code }}">
+                    <label for="{{ field.code }}">{{ field.title }}</label>
+                    <textarea class="form-control" name="{{ field.code }}" id="{{ field.code }}" placeholder="{{ field.placeholder }}" rows="7"></textarea>
+                </div>
 
-			{# SELECT #}
-			{% elseif field.type == 'select' %}
-				<div class="form-group field-{{ field.code }}">
-					<label for="{{ field.code }}">{{ field.title }}</label>
-					<select
-						class="form-control"
-						name="{{ field.code }}"
-						id="{{ field.code }}"
-					>
-						<option value="">{{ field.placeholder }}</option>
-						{% for option in field.options %}
-							<option value="{{ option.value }}">{{ option.title }}</option>
-						{% endfor %}
-					</select>
-				</div>
-			{# /SELECT #}
+            {% elseif field.type == 'select' %}
+                <div class="form-group field-{{ field.code }}">
+                    <label for="{{ field.code }}">{{ field.title }}</label>
+                    <select class="form-control" name="{{ field.code }}" id="{{ field.code }}">
+                        <option value="">{{ field.placeholder }}</option>
+                        {% for option in field.options %}
+                            <option value="{{ option.value }}">{{ option.title }}</option>
+                        {% endfor %}
+                    </select>
+                </div>
 
-			{# RECAPTCHA #}
-			{% elseif field.type == 'recaptcha' %}
-    		{% if recaptcha and recaptcha.site_key is not empty %}
-        		<div class="form-group field-recaptcha">
-            		<div class="g-recaptcha" data-sitekey="{{ recaptcha.site_key }}"></div>
-        		</div>
-        		<script src="https://www.google.com/recaptcha/api.js" async defer></script>
-    		{% else %}
-        		<div class="alert alert-danger">⚠️ reCAPTCHA sitekey не передано. Віджет не працюватиме.</div>
-    		{% endif %}
-			{% endif %}
-			{# /RECAPTCHA #}
-		
-			{# RADIO #}
-			{% elseif field.type == 'radio' %}
-				<div class="form-group" class="field-radio">
-					{% for option in field.options %}
-						<label class="radio-inline">
-							<input type="radio"
-								   name="{{ field.code }}"
-								   value="{{ option.value }}"
-								   id="{{ field.code ~ '_' ~ loop.index }}">
-							{{ option.title }}
-						</label>
-					{% endfor %}
-				</div>
-			{# /RADIO #}
+            {% elseif field.type == 'recaptcha' %}
+                {% if recaptcha and recaptcha.site_key is not empty %}
+                    <input type="hidden" name="g-recaptcha-response" value="">
+                    <script src="https://www.google.com/recaptcha/api.js?render={{ recaptcha.site_key }}"></script>
+                    <script>
+                        grecaptcha.ready(function () {
+                            grecaptcha.execute('{{ recaptcha.site_key }}', {action: '{{ recaptcha.action|default('submit') }}'}).then(function (token) {
+                                const form = document.getElementById('form-{{ form.code }}');
+                                if (form) {
+                                    let input = form.querySelector('[name="g-recaptcha-response"]');
+                                    if (!input) {
+                                        input = document.createElement('input');
+                                        input.type = 'hidden';
+                                        input.name = 'g-recaptcha-response';
+                                        form.appendChild(input);
+                                    }
+                                    input.value = token;
+                                }
+                            });
+                        });
+                    </script>
+                {% else %}
+                    <div class="alert alert-danger">⚠️ reCAPTCHA sitekey не передано. Віджет не працюватиме.</div>
+                {% endif %}
 
-			{# CHECKBOX #}
-			{% elseif field.type == 'checkbox' %}
-				<div class="form-group form-check field-{{ field.code }}">
-					<input
-						class="form-check-input"
-						type="checkbox"
-						name="{{ field.code }}"
-						id="{{ field.code ~ '_' ~ loop.index }}">
-					<label class="form-check-label" for="{{ field.code ~ '_' ~ loop.index }}">
-						{{ field.title }}
-					</label>
-				</div>
-			{# /CHECKBOX #}
+            {% elseif field.type == 'radio' %}
+                <div class="form-group field-radio">
+                    {% for option in field.options %}
+                        <label class="radio-inline">
+                            <input type="radio" name="{{ field.code }}" value="{{ option.value }}" id="{{ field.code ~ '_' ~ loop.index }}">
+                            {{ option.title }}
+                        </label>
+                    {% endfor %}
+                </div>
 
-			{# MULTICHECKBOX #}
-			{% elseif field.type == 'multicheckbox' %}
-				<div class="form-group form-check field-{{ field.code }}">
-					<label for="{{ field.code }}">{{ field.title }}</label>
-					{% for option in field.options %}
-						<label class="radio-inline">
-							<input type="checkbox"
-								   name="{{ field.code }}[]"
-								   value="{{ option.value }}"
-								   id="{{ field.code ~ '_' ~ loop.index }}">
-							{{ option.title }}
-						</label>
-					{% endfor %}
-				</div>
-			{# /MULTICHECKBOX #}
+            {% elseif field.type == 'checkbox' %}
+                <div class="form-group form-check field-{{ field.code }}">
+                    <input class="form-check-input" type="checkbox" name="{{ field.code }}" id="{{ field.code ~ '_' ~ loop.index }}">
+                    <label class="form-check-label" for="{{ field.code ~ '_' ~ loop.index }}">{{ field.title }}</label>
+                </div>
 
-			{# LABEL#}
+            {% elseif field.type == 'multicheckbox' %}
+                <div class="form-group form-check field-{{ field.code }}">
+                    <label for="{{ field.code }}">{{ field.title }}</label>
+                    {% for option in field.options %}
+                        <label class="radio-inline">
+                            <input type="checkbox" name="{{ field.code }}[]" value="{{ option.value }}" id="{{ field.code ~ '_' ~ loop.index }}">
+                            {{ option.title }}
+                        </label>
+                    {% endfor %}
+                </div>
+
             {% elseif field.type == 'label' %}
-				<div class="form-group">
-					<label>
-                        {{ field.title }}
-					</label>
-				</div>
-			{# /LABEL#}
+                <div class="form-group">
+                    <label>{{ field.title }}</label>
+                </div>
 
-			{# LABEL#}
             {% elseif field.type == 'file' %}
-				<div class="form-group form-check field-{{ field.code }}">
-					<input
-						class="form-check-input"
-						type="file"
-						name="{{ field.code }}"
-						id="{{ field.code ~ '_' ~ loop.index }}">
-					<label class="form-check-label" for="{{ field.code ~ '_' ~ loop.index }}">
-						{{ field.title }}
-					</label>
-				</div>
-			{# /LABEL#}
+                <div class="form-group form-check field-{{ field.code }}">
+                    <input class="form-check-input" type="file" name="{{ field.code }}" id="{{ field.code ~ '_' ~ loop.index }}">
+                    <label class="form-check-label" for="{{ field.code ~ '_' ~ loop.index }}">{{ field.title }}</label>
+                </div>
+            {% endif %}
+        {% endfor %}
 
-			{% endif %}
-		{% endfor %}
-
-		<button type="submit" class="btn btn-primary">{{ form.submit_text }}</button>
-	</form>
-	<div class="callback-form-result"></div>
+        <button type="submit" class="btn btn-primary">{{ form.submit_text }}</button>
+    </form>
+    <div class="callback-form-result"></div>
 </div>
